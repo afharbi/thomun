3cf8fc6d9fe688f61fb9e3b908a35d537b6b5f14
Ali Alharbi <afalharbi@jahez.net>
2025-07-18 19:49:32 +0300

    updated api signature: added pagination

--- app/src/main/java/com/example/thomun/data/api/ThomunApi.kt
+++ app/src/main/java/com/example/thomun/data/api/ThomunApi.kt
@@ -2,11 +2,14 @@ package com.example.thomun.data.api
 
 import com.example.thomun.data.models.HomeSectionsDto
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ThomunApi {
 
     @GET("home_sections")
-    suspend fun getHomeSections(): HomeSectionsDto
+    suspend fun getHomeSections(
+        @Query("page") page: Int
+    ): HomeSectionsDto
 
     @GET("https://mock.apidog.com/m1/735111-711675-default/search")
     suspend fun search(): HomeSectionsDto
