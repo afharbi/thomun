6687d9eb41dba3fd66e70de941f4fab6f9ecac80
Ali Alharbi <afalharbi@jahez.net>
2025-07-19 02:50:42 +0300

    handle navigation using compose-navigation

--- app/src/main/java/com/example/thomun/MainActivity.kt
+++ app/src/main/java/com/example/thomun/MainActivity.kt
@@ -7,11 +7,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.thomun.presentation.home.HomeScreen
+import com.example.thomun.presentation.search.SearchScreen
+import com.example.thomun.presentation.splash.SplashScreen
 import com.example.thomun.ui.theme.ThomunTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -22,26 +24,33 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ThomunTheme {
+                val navController = rememberNavController()
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    HomeScreen(modifier = Modifier.padding(innerPadding))
+                    NavHost(
+                        navController = navController,
+                        startDestination = "home",
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        composable("splash") {
+                            SplashScreen(onAnimationFinished = {
+                                navController.navigate("home") {
+                                    popUpTo("splash") { inclusive = true }
+                                }
+                            })
+                        }
+                        composable("home") {
+                            HomeScreen(
+                                onSearchClick = {
+                                    navController.navigate("search")
+                                }
+                            )
+                        }
+                        composable("search") {
+                            SearchScreen()
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ThomunTheme {
-        Greeting("Android")
     }
 }
