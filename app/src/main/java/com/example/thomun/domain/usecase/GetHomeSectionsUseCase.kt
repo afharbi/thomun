bdff39427b4b4f9ac0b8c0e3687d30622168944c
Ali Alharbi <afalharbi@jahez.net>
2025-07-19 02:52:29 +0300

    handle pagination in domain & presentation layers

--- app/src/main/java/com/example/thomun/domain/usecase/GetHomeSectionsUseCase.kt
+++ app/src/main/java/com/example/thomun/domain/usecase/GetHomeSectionsUseCase.kt
@@ -1,8 +1,11 @@
 package com.example.thomun.domain.usecase
 
+import androidx.paging.PagingData
+import com.example.thomun.domain.models.Section
 import com.example.thomun.domain.repository.HomeRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetHomeSectionsUseCase @Inject constructor(private val repository: HomeRepository) {
-    suspend fun invoke() = repository.getHomeSections()
+    fun getPagedSections(): Flow<PagingData<Section>> = repository.getPagedSections()
 }
--- app/src/main/java/com/example/thomun/presentation/home/HomeEvent.kt
+++ app/src/main/java/com/example/thomun/presentation/home/HomeEvent.kt
@@ -1,6 +1,5 @@
 package com.example.thomun.presentation.home
 
 sealed class HomeEvent {
-    data object LoadHome : HomeEvent()
     data class ShowError(val message: String) : HomeEvent()
 } 
